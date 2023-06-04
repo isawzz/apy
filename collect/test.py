1f480dd5a822778777465b887dcb4c41b9694524
isawzz <iwgajam@gmail.com>
2023-06-04 07:54:35 -0700

    das

--- collect/test.py
+++ collect/test.py
@@ -1,5 +1,9 @@
 from utils import Colors
 
+def test():
+	Colors.print("TEST", color='green', bg=True, end="!!!")
+
 Colors.print("halli hallo", color='red', bg=True, end="!!!")
+test()
 # printc("hallo", "Red", True)
 # printc('super ding',colors.CornflowerBlue)
