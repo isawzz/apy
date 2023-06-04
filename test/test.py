f4bcc9e06b614bd87da6a64793e9fa64e706a72e
isawzz <iwgajam@gmail.com>
2023-06-04 09:44:00 -0700

    das

---
+++ test/test.py
@@ -0,0 +1,26 @@
+
+#%%
+def f1(a,b):
+  return a+b
+
+def f2():
+  print('das ist mit ein raetsel warum das nicht geht!!!')
+  if (True):
+    print('true')
+  elif (23 == 23):
+    print('false')
+  else:
+    print('neither!!!')
+
+print('hallo')
+print(f1(12,13))
+
+
+
+
+
+
+
+
+
+
