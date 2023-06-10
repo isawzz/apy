b3522c8e6b38cf74a09e36807ee823695bae4f25
isawzz <iwgajam@gmail.com>
2023-06-10 08:50:03 -0700

    das

---
+++ test/sklearn.py
@@ -0,0 +1,9 @@
+from sklearn import datasets
+from sklearn import svm
+import matplotlib.pyplot as plt
+digits = datasets.load_digits()
+clf = svm.SVC(gamma=0.001,C=100.)
+clf.fit(digits.data[:-2], digits.target[:-2])
+plt.imshow(digits.data[-2].reshape((8,8)), cmap='Blues')
+print('predicted number:',clf.predict([digits.data[-2]])[0])
+plt.show()
